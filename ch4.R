5b2833f279ca3c5a9e03cdd5085cdd691d97b5d4
alex8937 <ultraseven8937@gmail.com>
2018-02-25 01:50:33 -0500

    added ch5.R

--- ch4.R
+++ ch4.R
@@ -52,6 +52,6 @@ library(class)
 attach(Smarket)
 Xlag=cbind(Lag1,Lag2)
 train=Year<2005
-knn.pred=knn(Xlag[train,],Xlag[!train,],Direction[train],k=1)
+knn.pred=knn(Xlag[train,],Xlag[!train,],Direction[train],k=3)
 table(knn.pred,Direction[!train])
 mean(knn.pred==Direction[!train])
